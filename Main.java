6da824d7040d20bfe85cb100b9f2de3544a41a97
andre2521 <andre2090621@gmail.com>
2026-03-27 19:00:31 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá, turma!");
+        System.out.println("EStou usando Java no GitHub Codespace");
+    }
+}
