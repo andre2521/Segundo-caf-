431132bf76d47da77bfadaec95489365ee026746
andre2521 <andre2090621@gmail.com>
2026-05-12 20:37:56 -0300

    Aula Java
    colocando em dia

--- Main.java
+++ Main.java
@@ -4,3 +4,46 @@ public class Main {
         System.out.println("EStou usando Java no GitHub Codespace");
     }
 }
+
+// POO -> Promoção Orientada Objeto
+// É uma linguagem Fortemente tipada.
+// Independente de plataforma(Multiplataforma).
+// JVM -> Java Virtual Machine 
+
+// A JVM é responsável por realizar a interpretação 
+// do bytecode e pela execução.
+
+// Uma variável é um espaço na mémoria onde podemos 
+// armazenar valores.
+
+// Tipos primitivos
+//byte 8bits -> 128 a 127
+//short 16bits -> -32.768. a 32.767
+//int 32bits -> -2.147.483.648 a 2.147.483.647
+//long 64bits -> -9.223.372.036.854.775.808 a 9.223.372.036.854.775.808
+public class main {
+
+public static void main(String[] args) {
+
+     int minhaIdade = 16;
+    String teste = "Alo Mundo";
+    var testando = "André Silva T. de Sousa"
+    var teste2 = 52;
+    byte n1 = 127;
+    testando = "Fernanda";
+    // Declarar uma variável -> tipo variavel = valor 
+    // Valor da variável usando o var ele
+    //Ira inferir o tipo automaticamente.
+
+    System.out.prinln(teste);
+    System.out.prinln(minhaIdade);
+}
+
+
+
+
+}
+
+class carro{
+    
+}
